4903f8b5b3c1ab85c38ecfbfa2de69f5d14a2642
Mohammed Ashraf <Muhammed.ashraf.elsayed@gmail.com>
2018-02-15 11:36:54 +0200

    rethink adnd second thought

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var db = require("second-thought");
+var assert = require("assert");
 module.exports = function(grunt){
     grunt.initConfig({
         jshint:{
@@ -7,6 +9,16 @@ module.exports = function(grunt){
             files:['lib/**/*.js', 'models/**/*.js'],
             tasks:['jshint']
         }
+    });
+    grunt.registerTask("installdb",function(){
+var done = this.async();
+db.connect({db:"membership"},function(err,db){
+    db.install(['users','logs','sessions'],function(err,tableResult){
+assert.ok(err===null,err);
+console.log('DB Installed:'+tableResult);
+done();
+    });
+});
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
